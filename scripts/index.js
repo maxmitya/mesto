910d7b3ff877e2dbff3e92a402416610c850a504
maxmitya <maksmityashin@gmail.com>
2023-07-29 20:46:17 +0300

    пофиксил попап развернутой картинки

--- scripts/index.js
+++ scripts/index.js
@@ -1,103 +1,43 @@
-const openPopupEdit = document.querySelector('#open-popup-edit'); //Переменная кнопки открытия попапа редактирования профиля
+//Переменная попапа редактирования профиля
+const editPopup = document.querySelector('#edit-popup');
 
-const openPopupAdd = document.querySelector('#open-popup-add'); //Переменная кнопки открытия попапа добавления карточки
+//Переменная попапа добавления карточки
+const addPopup = document.querySelector('#add-popup');
 
-const closePopupEdit = document.querySelector('#close-popup-edit'); //Переменная кнопки закрытия попапа редактирования профиля
+//Переменная имени профиля
+const profileName = document.querySelector('.profile__name');
 
-const closePopupAdd = document.querySelector('#close-popup-add'); //Переменная кнопки закрытия попапа добавления карточки
+//Переменная проффесии профиля
+const profileProfession = document.querySelector('.profile__profession');
 
-const editPopup = document.querySelector('#edit-popup'); //Переменная попапа редактирования профиля
+//Переменная поля ввода имени попапа редактирования
+const nameInput = document.querySelector('#name-input');
 
-const addPopup = document.querySelector('#add-popup'); //Переменная попапа добавления карточки
+//Переменная поля ввода проффессии попапа редактирования
+const professionInput = document.querySelector('#profession-input');
 
-const profileName = document.querySelector('.profile__name'); //Переменная имени профиля
+//Переменная поля ввода заголовка попапа добавления карточки
+const titleInput = document.querySelector('#title-input');
 
-const profileProfession = document.querySelector('.profile__profession'); //Переменная проффесии профиля
+//Переменная поля ввода ссылки на изображение попапа добавления карточки
+const imageInput = document.querySelector('#image-input');
 
-const cardTitle = document.querySelector('.card-title'); //Переменная заголовка карточки
+//Переменная формы попапа редактирования профиля
+const editForm = document.querySelector('#edit-form');
 
-const cardImage = document.querySelector('.card__image'); //Переменная изображения карточки
+//Переменная формы попапа добавления карточки
+const addForm = document.querySelector('#add-form');
 
-const nameInput = document.querySelector('#name-input'); //Переменная поля ввода имени попапа редактирования
+//Переменная заготовки (template) новой карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-const professionInput = document.querySelector('#profession-input'); //Переменная поля ввода проффессии попапа редактирования
+//Переменная карточки из template
+const cardFromTemplate = cardTemplate.querySelector('.card');
 
-const titleInput = document.querySelector('#title-input'); //Переменная поля ввода заголовка попапа добавления карточки
+//Переменная секции с карточками
+const cards = document.querySelector('.cards');
 
-const imageInput = document.querySelector('#image-input'); //Переменная поля ввода ссылки на изображение попапа добавления карточки
-
-const editForm = document.querySelector('#edit-form'); //Переменная формы попапа редактирования профиля
-
-const addForm = document.querySelector('#add-form'); //Переменная формы попапа добавления карточки
-
-const cardTemplate = document.querySelector('#card-template'); //Переменная заготовки (template) новой карточки
-
-const cardTemplateContent = cardTemplate.content; //Переменная контента заготовки (template) новой карточки
-
-const card = cardTemplateContent.querySelector('.card'); //Переменная карточки из template
-
-const cards = document.querySelector('.cards'); //Переменная секции с карточками
-
-function openPopupFunction(popup) {
-  //Функция открытия попапа
-  popup.classList.add('popup_opened');
-}
-
-function closePopupFunction(popup) {
-  //Функция закрытия попапа
-  popup.classList.remove('popup_opened');
-}
-
-openPopupEdit.addEventListener('click', function () {
-  //Слушатель нажатия на кнопку открытия попапа редактирования
-  openPopupFunction(editPopup);
-
-  nameInput.value = profileName.textContent;
-
-  professionInput.value = profileProfession.textContent;
-});
-
-openPopupAdd.addEventListener('click', function () {
-  //Слушатель нажатия на кнопку открытия попапа добавления
-  openPopupFunction(addPopup);
-});
-
-closePopupEdit.addEventListener('click', function () {
-  //Слушатель нажатия на кнопку закрытия попапа редактирования
-  closePopupFunction(editPopup);
-});
-
-closePopupAdd.addEventListener('click', function () {
-  //Слушатель нажатия на кнопку закрытия попапа добавления
-  closePopupFunction(addPopup);
-});
-
-editForm.addEventListener('submit', function (event) {
-  //Слушатель нажатия на кнопку Enter в форме попапа редактирования
-  event.preventDefault();
-
-  profileProfession.textContent = professionInput.value;
-
-  profileName.textContent = nameInput.value;
-
-  closePopupFunction(editPopup);
-});
-
-addForm.addEventListener('submit', function (event) {
-  //Слушатель нажатия на кнопку Enter в форме попапа создания новой карточки
-  event.preventDefault();
-
-  const newCard = card.cloneNode(true);
-  const cardTitle = newCard.querySelector('.card__title');
-  const cardImg = newCard.querySelector('.card__image');
-  cardTitle.textContent = titleInput.value;
-  cardImg.src = imageInput.value;
-
-  cards.prepend(newCard);
-
-  closePopupFunction(addPopup);
-});
-
+// Массив с новыми карточками
 const initialCards = [
   {
     name: 'Архыз',
@@ -125,24 +65,140 @@ const initialCards = [
   }
 ];
 
-initialCards.forEach(function (item) {
-  const newCard = createCard(item);
-  cards.prepend(newCard);
+//Функция открытия попапа
+function openPopupFunction(popup) {
+  popup.classList.add('popup_opened');
+}
+
+//Функция закрытия попапа
+function closePopupFunction(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+//Переменная кнопки открытия попапа редактирования профиля
+const openPopupEdit = document.querySelector('#open-popup-edit');
+
+//Слушатель нажатия на кнопку открытия попапа редактирования
+openPopupEdit.addEventListener('click', function () {
+  openPopupFunction(editPopup);
+
+  nameInput.value = profileName.textContent;
+
+  professionInput.value = profileProfession.textContent;
 });
 
-function createCard(item) {
-  const newCard = card.cloneNode(true);
+//Переменная кнопки закрытия попапа редактирования профиля
+const closePopupEdit = document.querySelector('#close-popup-edit');
+
+//Слушатель нажатия на кнопку закрытия попапа редактирования
+closePopupEdit.addEventListener('click', function () {
+  closePopupFunction(editPopup);
+});
+
+//Слушатель нажатия на кнопку Enter в форме попапа редактирования
+editForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  profileProfession.textContent = professionInput.value;
+
+  profileName.textContent = nameInput.value;
+
+  closePopupFunction(editPopup);
+});
+
+//Переменная кнопки открытия попапа добавления карточки
+const openPopupAdd = document.querySelector('#open-popup-add');
+
+//Слушатель нажатия на кнопку открытия попапа добавления
+openPopupAdd.addEventListener('click', function () {
+  openPopupFunction(addPopup);
+});
+
+//Переменная кнопки закрытия попапа добавления карточки
+const closePopupAdd = document.querySelector('#close-popup-add');
+
+//Слушатель нажатия на кнопку закрытия попапа добавления
+closePopupAdd.addEventListener('click', function () {
+  closePopupFunction(addPopup);
+});
+
+//Слушатель нажатия на кнопку Enter в форме попапа создания новой карточки
+addForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const newCard = cardFromTemplate.cloneNode(true);
+
   const cardTitle = newCard.querySelector('.card__title');
   const cardImg = newCard.querySelector('.card__image');
+
+  cardTitle.textContent = titleInput.value;
+  cardImg.src = imageInput.value;
+
+  const likeButton = newCard.querySelector('.card__button');
+  likeButton.addEventListener('click', function (event) {
+    event.target.classList.toggle('card__button_active');
+  });
+
+  const delButton = newCard.querySelector('.card__trash');
+  delButton.addEventListener('click', function (event) {
+    const element = event.target.closest('.card');
+    element.remove();
+  });
+
+  cards.prepend(newCard);
+
+  closePopupFunction(addPopup);
+});
+
+// Функция создания карточек из массива
+function createCard(item) {
+  const newCard = cardFromTemplate.cloneNode(true);
+
+  const cardTitle = newCard.querySelector('.card__title');
+  const cardImg = newCard.querySelector('.card__image');
+
   cardTitle.textContent = item.name;
   cardImg.src = item.link;
+
+  const delButton = newCard.querySelector('.card__trash');
+  delButton.addEventListener('click', function (event) {
+    const element = event.target.closest('.card');
+    element.remove();
+  });
+
+  const likeButton = newCard.querySelector('.card__button');
+  likeButton.addEventListener('click', function (event) {
+    event.target.classList.toggle('card__button_active');
+  });
+
+  // Переменная попапа увеличенной картинки
+  const imagePopup = document.querySelector('#image-popup');
+
+  // Переменная увеличенной картинки
+  const bigPopupImage = imagePopup.querySelector('.popup__img');
+
+  // Переменная заголовка увеличенной картинки
+  const bigPopupTitle = imagePopup.querySelector('.popup__title');
+
+  cardImg.addEventListener('click', function () {
+    imagePopup.classList.add('popup_opened');
+
+    bigPopupTitle.textContent = cardTitle.textContent;
+
+    bigPopupImage.src = cardImg.src;
+  });
+
+  const closeImagePopup = imagePopup.querySelector('#close-popup-image');
+
+  closeImagePopup.addEventListener('click', function () {
+    imagePopup.classList.remove('popup_opened');
+  });
 
   return newCard;
 }
 
-const likeButtons = document.querySelectorAll('.card__button');
-likeButtons.forEach(function (item) {
-  item.addEventListener('click', function () {
-    item.classList.toggle('card__button_active');
-  });
+// Метот перебора массива для создания карточек
+initialCards.forEach(function (item) {
+  const newCard = createCard(item);
+  cards.prepend(newCard);
 });
